88ca3bb513796ee31f6cd7139419c7ae026813f0
asakuracl <champi.ludo@hotmail.fr>
2019-08-25 17:46:35 -0300

    init textDraw & add drawing config in

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@ class canvasDraw {
         this.ctx = this.canvas.getContext("2d");
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
+        this.textDraw();
     }
 
     textDraw() {
-
+        this.ctx.strokeStyle = 'blue';
+        this.ctx.strokeRect(100, 100, 100, 100);
     }
 }
 
