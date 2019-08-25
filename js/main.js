797c7fb116c287f196bb67348e3bf30a80592b82
asakuracl <champi.ludo@hotmail.fr>
2019-08-25 20:33:21 -0300

    add this.x/yRect & this.x/yText

--- js/main.js
+++ js/main.js
@@ -7,14 +7,21 @@ class canvasDraw {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.textDraw();
+
+
     }
 
     textDraw() {
-        this.ctx.fillRect(0, 0, 100, 100);
+        this.xRect = 100;
+        this.yRect = 100;
+        this.xText = this.xRect - 60;
+        this.yText = this.yRect - 10;
+
+        this.ctx.fillRect(0, 0, this.xRect, this.yRect);
         this.ctx.font = "50pt Open sans";
         this.ctx.textBaseline = "up";
         this.ctx.fillStyle = "white";
-        this.ctx.fillText("A", 40, 90);
+        this.ctx.fillText("A", this.xText, this.yText);
     }
 }
 
