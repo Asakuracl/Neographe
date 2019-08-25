ca64bc887d91103442d132efefc6967adc05336f
asakuracl <champi.ludo@hotmail.fr>
2019-08-25 17:27:17 -0300

    add canvas width & height

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ class canvasDraw {
     constructor(canvas) {
         this.canvas = canvas;
         this.ctx = this.canvas.getContext("2d");
-
+        this.canvasWidth = window.innerWidth;
+        this.canvasHeight = window.innerHeight;
     }
 
     textDraw() {
