5421b4e70be917521ea059a185cb50cff33957b5
asakuracl <champi.ludo@hotmail.fr>
2019-08-26 22:56:18 -0300

    back to the first methode

--- js/main.js
+++ js/main.js
@@ -1,5 +1,92 @@
 // canvas
 
+// 1 methode
+
+class canvasDraw {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext("2d");
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+
+        this.letters = {
+            letter: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        };
+
+        this.letterValues = Object.values(this.letters.letter);
+
+        this.rectArray = [];
+
+        for (let i = 0; i < 100; i++) {
+            let rect = {
+                xPosRect: Math.random() * innerWidth,
+                yPosRect: Math.random() * innerHeight,
+                xRect: 100,
+                yRect: 100,
+                xText: this.xPosRect + 40,
+                yText: this.yPosRect + 90,
+                xSpeed: (Math.random() - 0.5) * 8,
+                ySpeed: (Math.random() - 0.5) * 8
+            };
+            rect.xText = rect.xPosRect + 40;
+            rect.yText = rect.yPosRect + 90;
+            rect.randomLetter = this.letterValues[Math.floor(Math.random() * this.letterValues.length)]
+            this.rectArray.push(rect);
+        };
+
+        console.log(this.rectArray);
+
+        this.animate();
+    }
+
+
+    textDraw(rect) {
+
+        this.ctx.beginPath();
+        this.ctx.strokeRect(rect.xPosRect, rect.yPosRect, rect.xRect, rect.yRect);
+        this.ctx.font = "50pt Open sans";
+        this.ctx.textBaseline = "up";
+        this.ctx.fillStyle = "black";
+        this.ctx.fillText(rect.randomLetter, rect.xText, rect.yText);
+    }
+
+
+    update(rect) {
+        if (rect.xPosRect + rect.xRect > innerWidth || rect.xPosRect < 0) {
+            rect.xSpeed = -rect.xSpeed;
+        }
+
+        if (rect.yPosRect + rect.yRect > innerHeight || rect.yPosRect < 0) {
+            rect.ySpeed = -rect.ySpeed;
+        }
+
+        rect.xPosRect += rect.xSpeed;
+        rect.xText += rect.xSpeed;
+        rect.yPosRect += rect.ySpeed;
+        rect.yText += rect.ySpeed;
+        this.textDraw(rect);
+    }
+
+
+    animate() {
+        requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+        for (let i = 0; i < this.rectArray.length; i++) {
+            this.update(this.rectArray[i]);
+        }
+    };
+
+}
+
+const canvas = document.querySelector("canvas");
+
+const draw = new canvasDraw(canvas);
+
+//2 methode 
+
+/*
 class canvasDraw {
     constructor(canvas) {
         this.canvas = canvas;
@@ -91,90 +178,4 @@ class canvasDraw {
 const canvas = document.querySelector("canvas");
 
 const draw = new canvasDraw(canvas);
-
-
-// 1 methode
-/*
-class canvasDraw {
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.ctx = this.canvas.getContext("2d");
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-
-        this.letters = {
-            letter: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        };
-
-        this.letterValues = Object.values(this.letters.letter);
-
-        this.rectArray = [];
-
-        for (let i = 0; i < 2; i++) {
-            let rect = {
-                xPosRect: Math.random() * innerWidth,
-                yPosRect: Math.random() * innerHeight,
-                xRect: 100,
-                yRect: 100,
-                xText: this.xPosRect + 40,
-                yText: this.yPosRect + 90,
-                xSpeed: (Math.random() - 0.5) * 8,
-                ySpeed: (Math.random() - 0.5) * 8
-            };
-            rect.xText = rect.xPosRect + 40;
-            rect.yText = rect.yPosRect + 90;
-            rect.randomLetter = this.letterValues[Math.floor(Math.random() * this.letterValues.length)]
-            this.rectArray.push(rect);
-        };
-
-        console.log(this.rectArray);
-
-        this.animate();
-    }
-
-
-    textDraw(rect) {
-
-        this.ctx.beginPath();
-        this.ctx.strokeRect(rect.xPosRect, rect.yPosRect, rect.xRect, rect.yRect);
-        this.ctx.font = "50pt Open sans";
-        this.ctx.textBaseline = "up";
-        this.ctx.fillStyle = "black";
-        this.ctx.fillText(rect.randomLetter, rect.xText, rect.yText);
-    }
-
-
-    update(rect) {
-        if (rect.xPosRect + rect.xRect > innerWidth || rect.xPosRect < 0) {
-            rect.xSpeed = -rect.xSpeed;
-        }
-
-        if (rect.yPosRect + rect.yRect > innerHeight || rect.yPosRect < 0) {
-            rect.ySpeed = -rect.ySpeed;
-        }
-
-        rect.xPosRect += rect.xSpeed;
-        rect.xText += rect.xSpeed;
-        rect.yPosRect += rect.ySpeed;
-        rect.yText += rect.ySpeed;
-        this.textDraw(rect);
-    }
-
-
-    animate() {
-        requestAnimationFrame(this.animate.bind(this));
-
-        this.ctx.clearRect(0, 0, innerWidth, innerHeight);
-
-        for (let i = 0; i < this.rectArray.length; i++) {
-            this.update(this.rectArray[i]);
-        }
-    };
-
-}
-
-const canvas = document.querySelector("canvas");
-
-const draw = new canvasDraw(canvas);
-
 */
