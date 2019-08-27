86ed0ea24adda48f162f4d9b1c0bebd6de779f7d
asakuracl <champi.ludo@hotmail.fr>
2019-08-27 10:59:55 -0300

    update x/yText position in square

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ class canvasDraw {
         this.ctx = this.canvas.getContext("2d");
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
-        this.squareNumber = 200;
+        this.squareNumber = 10;
 
         this.mouse = {
             x: undefined,
@@ -45,8 +45,8 @@ class canvasDraw {
             rect.xRect = Math.random() * 3 + 1;
             rect.yRect = rect.xRect;
             rect.minSize = rect.xRect;
-            rect.xText = rect.xPosRect + 40;
-            rect.yText = rect.yPosRect + 90;
+            rect.xText = rect.xPosRect - 50;
+            rect.yText = rect.yPosRect + 0;
             rect.randomLetter = this.letterValues[Math.floor(Math.random() * this.letterValues.length)];
             rect.randomColor = this.colorValues[Math.floor(Math.random() * this.colorValues.length)];
             this.rectArray.push(rect);
