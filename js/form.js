452358d8409014809f79b4f062efa0a68badae88
asakuracl <champi.ludo@hotmail.fr>
2019-09-21 00:00:36 -0300

    click event to show form

--- js/form.js
+++ js/form.js
@@ -1,3 +1,9 @@
 //hidden form
 const form = document.querySelector("#formul");
 form.style.display = "none";
+
+const touche = document.querySelector(".back");
+touche.addEventListener("click", function () {
+    form.style.display = "block";
+});
+
