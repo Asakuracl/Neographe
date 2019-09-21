2d6e552475bfca20f5d81354f5b495fd6766699a
asakuracl <champi.ludo@hotmail.fr>
2019-09-21 16:44:16 -0300

    event on in out form

--- js/form.js
+++ js/form.js
@@ -1,9 +1,7 @@
 //hidden form
-const form = document.querySelector("#formul");
-form.style.display = "none";
+const menu = document.querySelector(".menu");
+const form = document.querySelector(".container");
 
-const touche = document.querySelector(".back");
-touche.addEventListener("click", function () {
-    form.style.display = "block";
+menu.addEventListener("click", function () {
+    form.classList.toggle("inForm");
 });
-
