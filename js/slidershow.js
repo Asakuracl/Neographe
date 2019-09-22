b4997bc9bb2632fdaa9162793b2b9d7103122fe8
asakuracl <champi.ludo@hotmail.fr>
2019-09-22 17:48:36 -0300

    confiog auto launch

--- js/slidershow.js
+++ js/slidershow.js
@@ -2,24 +2,18 @@ class slideshow {
     constructor() {
 
         this.counter = 0;
+        this.temps = 3000;
 
-        //add groupe in bracket, change groupe name in quotation marks.
         this.textContainer = [
             "HTML/CSS",
-            "CMS",
-            "JAVASCRIPT"];
+            "JAVASCRIPT",
+            "CMS"];
 
         this.textIn = document.querySelector(".text-container");
-
-        this.move = document.querySelector("#square");
-        this.move.addEventListener("click", this.Next.bind(this));
-
-        document.addEventListener("keydown", this.keyboard.bind(this));
-
+        this.launch();
     }
 
     Next() {
-
         if (this.counter > this.textContainer.length - 1) {
             this.counter = 0;
         }
@@ -36,14 +30,9 @@ class slideshow {
         this.textIn.innerHTML = this.textContainer[this.counter];
     }
 
-
-    // Change text with keyboard
-    keyboard(e) {
-        if (e.keyCode === 39) {
-            this.Next();
-        } else if (e.keyCode === 37) {
-            this.Prev();
-        }
+    // Change auto
+    launch() {
+        this.interv = setInterval(this.Next.bind(this), this.temps);
     }
 }
 
